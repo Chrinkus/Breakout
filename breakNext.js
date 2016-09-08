e82eed0bf93dd61ce2d8e66b801ba504cf5973a2
Chrinkus <seeschickrun@gmail.com>
2016-09-08 11:32:34 -0500

    Bricks added w/collision & reaction

--- breakNext.js
+++ breakNext.js
@@ -4,9 +4,25 @@ BREAK_APP.canvas = document.getElementById("myCanvas");
 BREAK_APP.ctx = BREAK_APP.canvas.getContext("2d");
 
 BREAK_APP.entities = [];
+BREAK_APP.brickField = {
+    rowCount: 3,
+    columnCount: 5,
+    bricks: [],
+    fill: function() {
+        for (let c = 0; c < this.columnCount; c++) {
+            this.bricks[c] = [];
+            for (let r = 0; r < this.rowCount; r++) {
+                this.bricks[c][r] = new Brick(c, r);
+            }
+        }
+    }
+};
 BREAK_APP.init = function() {
     this.entities.push(new Ball()); 
     this.entities.push(new Paddle());
+
+    this.brickField.fill();
+    this.entities.push(this.brickField.bricks);
 };
 
 BREAK_APP.update = function () {
@@ -38,7 +54,7 @@ BREAK_APP.utils.clear = function() {
 BREAK_APP.utils.each = function(arr, f) {
     arr.forEach(function(ent) {
         if (ent.length && typeof ent === "object") {
-            return BREAK_APP.utils.each(ent);
+            return BREAK_APP.utils.each(ent, f);
         }
         f(ent);
     });
@@ -46,9 +62,9 @@ BREAK_APP.utils.each = function(arr, f) {
 
 BREAK_APP.utils.collision = function(entity, that) {
     // Next styled CD solution w/radius subbed in for circle tests
-    return that.x < entity.x + entity.w &&
+    return that.x - that.r < entity.x + entity.w &&
         that.x + that.r > entity.x &&
-        that.y < entity.y + entity.h &&
+        that.y - that.r < entity.y + entity.h &&
         that.y + that.r > entity.y;
 };
 
@@ -96,8 +112,16 @@ Ball.prototype.update = function() {
         // Omit self
         if (ent === that) { return; }
 
+        // Omit "hit" bricks
+        if (!ent.status) { return; }
+
         // Check each entity for collision with "this"
-        if (BREAK_APP.utils.collision(ent, that)){
+        if (BREAK_APP.utils.collision(ent, that)) {
+            // If brick, disappear
+            if (ent instanceof Brick) {
+                ent.status = 0;
+            }
+            //Change direction
             that.dy = -that.dy;
         }
     });
@@ -114,6 +138,7 @@ Ball.prototype.draw = function(ctx) {
 function Paddle(x, y, h, w, v) {
     "use strict";
     var that = this;
+    this.status = 1;
     this.x = x || (BREAK_APP.utils.CW - (w || 75)) / 2;
     this.y = y || (BREAK_APP.utils.CH - (h || 10));
     this.w = w || 75;
@@ -143,6 +168,40 @@ Paddle.prototype.draw = function(ctx) {
     Ball.prototype.draw.call(this, ctx);
     //ctx.fillStyle = this.color;
     //ctx.fill(this.path(this.x, this.y));
+};
+
+function Brick(cVal, rVal) {
+    "use strict";
+    var that = this;
+    this.status = 1;
+    this.w = 75;
+    this.h = 20;
+    this.padding = 10;
+    this.offsetTop = 30;
+    this.offsetLeft = 30;
+    this.x = (cVal * (this.w + this.padding)) + this.offsetLeft;
+    this.y = (rVal * (this.h + this.padding)) + this.offsetTop;
+    this.color = "#0095DD";
+    this.path = function() {
+        var path = new Path2D();
+        path.rect(that.x, that.y, that.w, that.h);
+        path.closePath();
+        return path;
+    };
+}
+
+Brick.prototype.update = function() {
+    // Nothing to update at this time
+};
+
+Brick.prototype.draw = function(ctx) {
+    //Ball.prototype.draw.call(this, ctx);
+    if (!this.status) {
+        // Don't draw missing bricks
+        return;
+    }
+    ctx.fillStyle = this.color;
+    ctx.fill(this.path());
 };
 
 (function () {
