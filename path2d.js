cedec364462925df87f13a0571b76e5246e992d4
Chrinkus <seeschickrun@gmail.com>
2016-09-28 12:02:26 -0500

    Experimenting with shapes

--- path2d.js
+++ path2d.js
@@ -1,3 +1,4 @@
+/*
 var BREAK_APP = BREAK_APP || {};
 
 // Incomplete, does nothing
@@ -15,3 +16,39 @@ var BREAK_APP = BREAK_APP || {};
     }
     this.ball.path.arc(x, y, r, 0, Math.PI * 2);
 })
+*/
+var circle = function(x, y, r) {
+    var path = new Path2D();
+    path.arc(x, y, r, 0, Math.PI * 2);
+    path.closePath();
+    return path;
+};
+
+var square = function(x, y, s) {
+    var path = new Path2D();
+    path.rect(x, y, s, s);
+    path.closePath();
+};
+
+var triangle = function(x, y, s) {
+    var path = new Path2D();
+    var h = Math.sin(60 * Math.PI / 180) * s;
+    path.moveTo(x, y + h);
+    path.lineTo(x + s / 2, y);
+    path.lineTo(x + s, y + h);
+    path.closePath();
+    return path;
+};
+
+var pentagon = function(x, y, d) {
+    var path = new Path2D();
+    var s = d / 2 / Math.cos(54 * Math.PI / 180);
+    var aH = d / 2 * Math.tan(54 * Math.PI / 180);
+    path.moveTo(x + d / 2, y);
+    path.lineTo(x + d, y + aH);
+    path.lineTo(x + d - aH, y + d);
+    path.lineTo(x + aH, y + d);
+    path.lineTo(x, y + aH);
+    path.closePath();
+    return path;
+};
